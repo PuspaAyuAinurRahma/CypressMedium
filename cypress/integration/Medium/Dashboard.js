5dfd0a21c7910879901b338a76b341bfeb571def
Puspa Ayu Ainur Rahma <puspaayurahma62@gmail.com>
2021-06-29 12:29:12 +0700

    Add cypress check body

--- cypress/integration/Medium/Dashboard.js
+++ cypress/integration/Medium/Dashboard.js
@@ -26,4 +26,42 @@ context('Actions', () => {
             .contains('It\'s easy and free to post your thinking on any topic and connect with millions of readers.');
         cy.get('.cx > span > .ca').should('be.visible');
     });
+    it('Body', () => {
+        cy.get('.ht > div.af > .ca').contains('Trending on Medium');
+
+        cy.get(':nth-child(1) > :nth-child(1) > .gw')
+            .should('be.visible');
+        cy.get(':nth-child(1) > :nth-child(1) > .gw > .gx > .ca')
+            .contains('1');
+
+        cy.get(':nth-child(2) > :nth-child(1) > .gw')
+            .should('be.visible');
+        cy.get(':nth-child(2) > :nth-child(1) > .gw > .gx > .ca')
+            .contains('2');
+
+        cy.get(':nth-child(3) > :nth-child(1) > .gw')
+            .should('be.visible');
+        cy.get(':nth-child(3) > :nth-child(1) > .gw > .gx > .ca')
+            .contains('3');
+
+        cy.get(':nth-child(4) > :nth-child(1) > .gw')
+            .should('be.visible');
+        cy.get(':nth-child(4) > :nth-child(1) > .gw > .gx > .ca')
+            .contains('4');
+
+        cy.get(':nth-child(5) > :nth-child(1) > .gw')
+            .should('be.visible');
+        cy.get(':nth-child(5) > :nth-child(1) > .gw > .gx > .ca')
+            .contains('5');
+
+        cy.get(':nth-child(6) > :nth-child(1) > .gw')
+            .should('be.visible');
+        cy.get(':nth-child(6) > :nth-child(1) > .gw > .gx > .ca')
+            .contains('6');
+
+        cy.get('.r > :nth-child(1) > :nth-child(1)')
+            .should('be.visible');
+        cy.get('.x > .ho')
+            .should('be.visible');
+    });
 });
